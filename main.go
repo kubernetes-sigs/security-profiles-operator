2bc6a0683c8db7b2351de6ba956cd4a01593f4ea
Daniel Mangum <31777345+hasheddan@users.noreply.github.com>
2020-05-26 11:05:58 -0500

    Add initial scaffolding for seccomp profile controller (#4)
    This sets up some initial scaffolding for reconciling
    config maps that specify themselves as seccomp profiles.
    The implementation simply filters config maps and logs
    the resource version of config maps with the seccomp
    profile annotation.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ main.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"os"
+	"time"
+
+	"k8s.io/klog/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/saschagrunert/seccomp-operator/controllers/profile"
+)
+
+var (
+	sync     = time.Second * 30
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func main() {
+	ctrl.SetLogger(klogr.New())
+
+	cfg, err := ctrl.GetConfig()
+	if err != nil {
+		setupLog.Error(err, "cannot get config")
+		os.Exit(1)
+	}
+
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
+		SyncPeriod: &sync,
+	})
+	if err != nil {
+		setupLog.Error(err, "cannot create manager")
+		os.Exit(1)
+	}
+
+	if err := profile.Setup(mgr, ctrl.Log.WithName("profile")); err != nil {
+		setupLog.Error(err, "cannot setup profile controller")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "controller manager error")
+		os.Exit(1)
+	}
+}
