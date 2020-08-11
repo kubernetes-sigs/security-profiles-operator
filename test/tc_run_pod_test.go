ad6f652297293e19f1adbfa2e64c8606ed6791bf
Sascha Grunert <sgrunert@suse.com>
2020-08-11 14:03:52 +0200

    tree b7fbdc8bf677e92e9cc2556c25e096f46a2f5ccc parent 48a2d9868c0c193914cbb84c75c2a875823eecc9 author Sascha Grunert <sgrunert@suse.com> 1597147432 +0200 committer Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com> 1597150576 -0700
    Split-up e2e test into test cases
    
    We now use dedicated functions and files for the test cases to be able
    keep the tests clean. Every test case expects a valid working operator
    to be deployed and is expected to cleanup on its own.
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

---
+++ test/tc_run_pod_test.go
@@ -0,0 +1,41 @@
+// +build e2e
+
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e_test
+
+func (e *e2e) testCaseRunPod([]string) {
+	const (
+		examplePodPath = "examples/pod.yaml"
+		examplePodName = "test-pod"
+	)
+
+	e.logf("Creating the test pod: %s", examplePodPath)
+	e.kubectl("create", "-f", examplePodPath)
+	defer e.kubectl("delete", "-f", examplePodPath)
+
+	e.logf("Waiting for test pod to be ready")
+	e.kubectl("wait", "--for", "condition=ready", "pod", "--all")
+
+	e.logf("Testing that `rmdir` is not possible inside the pod")
+	failureOutput := e.kubectlFailure(
+		"exec", examplePodName, "--", "rmdir", "/home",
+	)
+	e.Contains(failureOutput,
+		"rmdir: failed to remove '/home': Operation not permitted",
+	)
+}
