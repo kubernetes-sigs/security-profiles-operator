b292dd72053e9868d39e6d2063dcfb1f4454e99a
Sascha Grunert <sgrunert@suse.com>
2020-09-15 09:12:41 +0200

    Update kind to v0.9.0
    This also updates the kind image to the latest k8s v1.19.1 release.
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- test/suite_test.go
+++ test/suite_test.go
@@ -36,9 +36,9 @@ import (
 )
 
 const (
-	kindVersion = "v0.8.1"
-	kindSHA512  = "ff71adddbe043df84c4dee82f034c6856bfc51c931bb7839d9c09d02fca93bfe961a9c05d7c657371963cc81febee175133598bba50fb1481a9faa06b42abdc3" // nolint: lll
-	kindImage   = "kindest/node:v1.19.0"
+	kindVersion = "v0.9.0"
+	kindSHA512  = "e7152acf5fd7a4a56af825bda64b1b8343a1f91588f9b3ddd5420ae5c5a95577d87431f2e417a7e03dd23914e1da9bed855ec19d0c4602729b311baccb30bd7f" // nolint: lll
+	kindImage   = "kindest/node:v1.19.1"
 	testImage   = config.OperatorName + ":latest"
 )
 
