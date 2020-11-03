efe155e3ff7178be8a426e7ab4fe90015a9f4c09
Colleen Murphy <colleen.murphy@suse.com>
2020-11-02 17:37:32 -0800

    Add ability to merge seccomp profiles
    Add an attribute BaseProfileName to the SeccompProfile resource. When
    reconciling a SeccompProfile, the controller will look up a profile with
    this name in the same namespace and use it as a base upon which the
    syscalls of both resources are unioned. This allows users to be able to
    create a base profile for their container runtime and then add only the
    syscalls needed for a particular application.
    
    The base profile lookup is not recursive, so only two profiles can be
    combined.
    
    The controller only combines syscalls, not flags or architectures or the
    default action. It does not validate whether a syscall is specified for
    multiple contrary actions.

---
+++ test/tc_base_profiles_test.go
@@ -0,0 +1,98 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e_test
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func (e *e2e) testCaseBaseProfile([]string) {
+	const baseProfilePath = "examples/baseprofile.yaml"
+	const helloProfile = `
+apiVersion: security-profiles-operator.x-k8s.io/v1alpha1
+kind: SeccompProfile
+metadata:
+  name: hello
+spec:
+  defaultAction: SCMP_ACT_ERRNO
+  targetWorkload: hello
+  baseProfileName: runc-v1.0.0-rc92
+  syscalls:
+  - action: SCMP_ACT_ALLOW
+    names:
+    - arch_prctl
+    - set_tid_address
+    - exit_group
+`
+	const helloPod = `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: hello
+spec:
+  containers:
+  - image: hello-world
+    name: hello
+    resources: {}
+  securityContext:
+    seccompProfile:
+      type: Localhost
+      localhostProfile: operator/default/hello/hello.json
+  restartPolicy: Never
+`
+	e.kubectl("create", "-f", baseProfilePath)
+	defer e.kubectl("delete", "-f", baseProfilePath)
+
+	e.logf("Creating hello profile")
+	helloProfileFile, err := ioutil.TempFile(os.TempDir(), "hello-profile*.yaml")
+	e.Nil(err)
+	defer os.Remove(helloProfileFile.Name())
+	_, err = helloProfileFile.Write([]byte(helloProfile))
+	e.Nil(err)
+	err = helloProfileFile.Close()
+	e.Nil(err)
+	e.kubectl("create", "-f", helloProfileFile.Name())
+
+	e.logf("Creating hello-world pod")
+	helloPodFile, err := ioutil.TempFile(os.TempDir(), "hello-pod*.yaml")
+	e.Nil(err)
+	defer os.Remove(helloPodFile.Name())
+	_, err = helloPodFile.Write([]byte(helloPod))
+	e.Nil(err)
+	err = helloPodFile.Close()
+	e.Nil(err)
+	e.kubectl("create", "-f", helloPodFile.Name())
+	defer e.kubectl("delete", "pod", "hello")
+
+	e.logf("Waiting for test pod to be initialized")
+	e.kubectl("wait", "--for", "condition=initialized", "pod", "hello")
+
+	output := e.kubectl("get", "pod", "hello")
+	for strings.Contains(output, "ContainerCreating") {
+		output = e.kubectl("get", "pod", "hello")
+	}
+
+	e.logf("Testing that container is launched without runtime permission errors")
+	output = e.kubectl("describe", "pod", "hello")
+	e.NotContains(output, "Error: failed to start containerd task")
+
+	e.logf("Testing that container ran successfully")
+	output = e.kubectl("logs", "hello")
+	e.Contains(output, "Hello from Docker!")
+}
