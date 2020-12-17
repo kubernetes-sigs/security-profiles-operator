7adee825dea0d20d4e163d15ef6afc5d57cd9ede
Colleen Murphy <colleen.murphy@suse.com>
2020-12-17 12:32:04 -0800

    Reorganize CRD type definitions
    controller-gen is nondeterministic about the order in which it generates
    YAML manifests from Go types, so `make manifests` may put them in a
    different order and cause unnecessary changes to the generated YAML
    files. The code is growing to a point that a little reorganization is
    nice anyway. This change splits the types into their own packages so
    that the Makefile command can have more fine-grained control over which
    types to generate in which order.

--- internal/pkg/controllers/selinuxpolicy/selinuxpolicy_controller.go
+++ internal/pkg/controllers/selinuxpolicy/selinuxpolicy_controller.go
@@ -34,7 +34,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	spov1alpha1 "sigs.k8s.io/security-profiles-operator/api/v1alpha1"
+	spov1alpha1 "sigs.k8s.io/security-profiles-operator/api/selinuxpolicy/v1alpha1"
 )
 
 // The underscore is not a valid character in a pod, so we can
