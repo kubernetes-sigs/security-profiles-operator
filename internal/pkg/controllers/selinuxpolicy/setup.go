0c18258671fb00aba3f4ba78ef62b3c592a96e5b
Jakub Hrozek <jhrozek@redhat.com>
2021-01-12 12:24:50 +0100

    Take selinuxd into use
    Changes SELinux integration by ditching the intermediate configMaps
    previously used by the CM reconcile loop of the selinux controller.
    Instead, the daemon running in the DS now takes an boolean argument that
    signifies if the SELinux feature should be enabled. This is false by
    default.
    
    If true, the DS deploys an additional container running selinuxd which
    listens to any files under /etc/selinux.d and can be queried for status
    over a UNIX socket mounted under /var/run/selinuxd. Both the socket
    directory and the drop-in directory are EmptyDirs shared between
    selinuxd and the controller.
    
    In addition, the operator receives a --with-selinux=true flag, which
    makes the operator create an additional manager that listens for the
    selinuxpolicy API object. Contents of those objects are reconciled to
    the /etc/selinux.d drop-in directory and then the controller queries
    the state of selinuxd to make sure the policy was indeed added or
    removed.

--- internal/pkg/controllers/selinuxpolicy/setup.go
+++ internal/pkg/controllers/selinuxpolicy/setup.go
@@ -21,38 +21,18 @@ import (
 	"text/template"
 
 	"github.com/crossplane/crossplane-runtime/pkg/event"
-	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	spov1alpha1 "sigs.k8s.io/security-profiles-operator/api/selinuxpolicy/v1alpha1"
-	"sigs.k8s.io/security-profiles-operator/internal/pkg/config"
 )
 
 var log = logf.Log.WithName("selinuxpolicy")
 
-// TODO(jaosorior): Use better method than a label constant.
-const cmIsSelinuxPolicy = "is-selinux-policy"
-
 // Setup adds a controller that reconciles seccomp profiles.
 func Setup(ctx context.Context, mgr ctrl.Manager, l logr.Logger) error {
-	if err := ctrl.NewControllerManagedBy(mgr).
-		Named("configmaps").
-		For(&corev1.ConfigMap{}).
-		WithEventFilter(resource.NewPredicates(hasSELinuxLabel)).
-		Owns(&corev1.Pod{}).
-		Complete(&ReconcileConfigMap{
-			client: mgr.GetClient(),
-			scheme: mgr.GetScheme(),
-			record: event.NewAPIRecorder(mgr.GetEventRecorderFor("configmaps")),
-		}); err != nil {
-		return errors.Wrap(err, "creating configmap controller")
-	}
-
 	// Create template to wrap policies
 	tmpl, err := template.New("policyWrapper").Parse(policyWrapper)
 	if err != nil {
@@ -68,24 +48,4 @@ func Setup(ctx context.Context, mgr ctrl.Manager, l logr.Logger) error {
 			policyTemplate: tmpl,
 			record:         event.NewAPIRecorder(mgr.GetEventRecorderFor("selinuxpolicy")),
 		})
-}
-
-func hasSELinuxLabel(obj runtime.Object) bool {
-	cm, ok := obj.(*corev1.ConfigMap)
-	if !ok {
-		return false
-	}
-	// we only care about this namespace
-	if cm.GetNamespace() != config.GetOperatorNamespace() {
-		return false
-	}
-	labels := cm.GetLabels()
-	if labels == nil {
-		return false
-	}
-	val, ok := labels[cmIsSelinuxPolicy]
-	if !ok {
-		return false
-	}
-	return val == "true"
 }
