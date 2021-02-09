d5d7c6ba0bafe7c804d043293b87827f888890c7
Sascha Grunert <mail@saschagrunert.de>
2021-02-09 11:27:55 +0100

    tree 88ab93171a3b3c57c1342a85784af42a59bee329 parent d59b0e8c8265f067d2a33a003e74077ee7b841c7 author Sascha Grunert <mail@saschagrunert.de> 1612866475 +0100 committer Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com> 1612949939 -0800
    Add basic Vagrant test job
    
    This test job can be later on used to run e2e tests within a fedora
    Vagrant box.
    
    Signed-off-by: Sascha Grunert <mail@saschagrunert.de>

---
+++ hack/ci/env.sh
@@ -0,0 +1,29 @@
+# Copyright 2021 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+local_ip() {
+    ip route get 1.2.3.4 | cut -d ' ' -f7 | tr -d '[:space:]'
+}
+
+GOPATH=$(go env GOPATH)
+
+export KUBE_CONTAINER_RUNTIME=remote
+export KUBERUN=/var/run/kubernetes
+export KUBECONFIG=$KUBERUN/admin.kubeconfig
+export PATH="$GOPATH/src/k8s.io/kubernetes/_output/bin:$PATH"
+
+IP=$(local_ip)
+export KUBE_MASTER_URL=$IP
+export KUBE_MASTER_IP=$IP
+export KUBE_MASTER=$IP
