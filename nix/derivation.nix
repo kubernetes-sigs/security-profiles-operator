86dd8997a1f1a359359086f0670eccfd620cd23a
Sascha Grunert <mail@saschagrunert.de>
2021-02-10 15:52:03 +0100

    tree ba599b5b9d3861fb60c54e14d1abb75587ed7152 parent 2fce3843fadbc7f1aa17ab5d9ef6e6dcf3e64c4a author Sascha Grunert <mail@saschagrunert.de> 1612968723 +0100 committer Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com> 1613048536 -0800
    Add arm64 binary cross compilation support
    
    We now utilize [nix](https://nixos.org) to be able to build the operator
    binary in a reproducible environment. This way we're able to cross-build
    the binary to any supported target architecture.
    
    For example the new `make nix` target now builds the locally available
    system binary, which should be usually x86-64:
    
    ```
    > make nix
    …
    file result/*
    result/security-profiles-operator: ELF 64-bit LSB executable, x86-64, version 1 (SYSV), statically linked, for GNU/Linux 2.6.32, Go BuildID=JPRHwsjKv1rgegLFOzeZ/MbtM6EjbKLpv2qaZ1bUS/OLZzjQg7BJIUB1yQ5x7L/aXXV7z2TB5tXSZBtwwvB, stripped
    ```
    
    Cross compilation works via the `make nix-arm64` target, too:
    
    ```
    
    > make nix-arm64
    …
    file result/*
    result/security-profiles-operator: ELF 64-bit LSB executable, ARM aarch64, version 1 (SYSV), statically linked, for GNU/Linux 2.6.32, Go BuildID=Vq3MhhbkzqJwPo6-4IUP/wZumN1S4dVZPxXo1vz6F/VFKjirA5ExnyxY3Nll47/JreX-K6WRUgMk3chrXSn, stripped
    ```
    
    Target is to replace the container image build in a next step to utilize
    both nix targets to create multi-arch container images.
    
    Signed-off-by: Sascha Grunert <mail@saschagrunert.de>

---
+++ nix/derivation.nix
@@ -0,0 +1,24 @@
+{ pkgs, buildGoModule }:
+with pkgs; buildGoModule rec {
+  name = "security-profiles-operator";
+  src = ./..;
+  vendorSha256 = null;
+  doCheck = false;
+  outputs = [ "out" ];
+  nativeBuildInputs = with buildPackages; [
+    git
+    pkg-config
+    which
+  ];
+  buildInputs = [
+    glibc
+    glibc.static
+    (libseccomp.overrideAttrs (x: { dontDisableStatic = true; }))
+  ];
+  buildPhase = ''
+    make
+  '';
+  installPhase = ''
+    install -Dm755 -t $out build/security-profiles-operator
+  '';
+}
