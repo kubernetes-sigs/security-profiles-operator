0c10cc145cf3e0f6382fa307bcc3789c8408a474
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2021-03-05 08:47:31 +0200

    Use Downward API to get operator namespace
    Instead of having the namespace be hardcoded, this uses the downward API
    to fetch it from the current namespace.
    
    Co-Authored-By: Naveen <172697+naveensrinivasan@users.noreply.github.com>
    Signed-off-by: Juan Antonio Osorio Robles <jaosorior@redhat.com>

---
+++ internal/pkg/config/config_test.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetOperatorNamespace(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "Valid one",
+			want:    "default",
+			wantErr: false,
+		},
+		{
+			name:    "invalid one",
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if tt.wantErr {
+				os.Unsetenv("OPERATOR_NAMESPACE")
+			} else {
+				os.Setenv("OPERATOR_NAMESPACE", tt.want)
+			}
+			got, err := TryToGetOperatorNamespace()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetOperatorNamespace() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetOperatorNamespace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
