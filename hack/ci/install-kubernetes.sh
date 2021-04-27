0a602ce450a7156460efaf2266a83c76366599a3
Sascha Grunert <sgrunert@redhat.com>
2021-04-27 10:06:12 +0200

    Bump VM based e2e test to k8s v1.21.0
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/ci/install-kubernetes.sh
+++ hack/ci/install-kubernetes.sh
@@ -19,7 +19,7 @@ ENVFILE=$(dirname "${BASH_SOURCE[0]}")/env.sh
 . "$ENVFILE"
 
 K8SPATH="$GOPATH/src/k8s.io"
-VERSION=v1.20.2
+VERSION=v1.21.0
 
 download-kubernetes() {
     export KUBERNETES_RELEASE=$VERSION
