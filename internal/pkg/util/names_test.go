18684103bc58e9fce149c4e75f1f85b56228ca9d
Jakub Hrozek <jhrozek@redhat.com>
2021-11-18 23:43:17 +0100

    secprofnodestatus: Link the node profile status with profile using a custom label
    We were using the profile name as the value of the spo.x-k8s.io/profile-name
    label to be able to list node profile statuses for a profile. This is
    user-friendly but would fail in case the profile name was longer than 64
    characters as that's the label size limit.
    
    Instead, let's rename the label to spo.x-k8s.io/profile-id. In case the
    profile name is shorter than the limit, let's still use the name because
    it's user-friendly, but in case the name is longer, let's instead use a
    sha1 hash of the name with the profile Kind as a prefix.
    
    Using sha1 is OK in this case, because we're not using it for any crypto
    purposes, just to shorten a name.

---
+++ internal/pkg/util/names_test.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	seccompprofile "sigs.k8s.io/security-profiles-operator/api/seccompprofile/v1beta1"
+)
+
+func TestNameHashing(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name      string
+		prof      seccompprofile.SeccompProfile
+		labelName string
+	}{
+		{
+			name: "short name",
+			prof: seccompprofile.SeccompProfile{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "SeccompProfile",
+					APIVersion: "security-profiles-operator.x-k8s.io/v1beta1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "shortname-profile",
+					Namespace: "security-profiles-operator",
+				},
+				Spec:   seccompprofile.SeccompProfileSpec{},
+				Status: seccompprofile.SeccompProfileStatus{},
+			},
+			labelName: "SeccompProfile-shortname-profile",
+		},
+		{
+			name: "long name",
+			prof: seccompprofile.SeccompProfile{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "SeccompProfile",
+					APIVersion: "security-profiles-operator.x-k8s.io/v1beta1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "this-is-a-very-long-name-surely-over-64-characters-omg-its-overflowing",
+					Namespace: "security-profiles-operator",
+				},
+				Spec:   seccompprofile.SeccompProfileSpec{},
+				Status: seccompprofile.SeccompProfileStatus{},
+			},
+			labelName: "SeccompProfile-9d42ecd8a72de861cc202ee69381e536088eec6dc43f8f8e",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			name := KindBasedDNSLengthName(&tc.prof)
+			require.Equal(t, tc.labelName, name)
+		})
+	}
+}
