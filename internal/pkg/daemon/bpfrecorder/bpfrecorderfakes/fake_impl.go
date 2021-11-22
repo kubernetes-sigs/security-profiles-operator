da20a791b3f32a2061c41cbfc6a886fdeebc1e00
Sascha Grunert <sgrunert@redhat.com>
2021-11-22 09:24:29 +0100

    Update repo-infra to v0.2.3
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- internal/pkg/daemon/bpfrecorder/bpfrecorderfakes/fake_impl.go
+++ internal/pkg/daemon/bpfrecorder/bpfrecorderfakes/fake_impl.go
@@ -1,7 +1,8 @@
+//go:build linux
 // +build linux
 
 /*
-Copyright 2021 The Kubernetes Authors.
+Copyright The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
