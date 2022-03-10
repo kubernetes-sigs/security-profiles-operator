df64d6e5ebddffa4e1b4c9ac5c9e7c909fa83da0
Sascha Grunert <sgrunert@redhat.com>
2022-03-10 12:52:41 +0100

    tree bb5a9638ebc769d61bbf6817e96517812c4c1cc7 parent dd72d74e37a41afa7084d9bf680a3b9dcc95666e author Sascha Grunert <sgrunert@redhat.com> 1646913161 +0100 committer Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com> 1647251900 -0700
    Bump CRI-O CI version and update script
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/ci/install-cri-o.sh
+++ hack/ci/install-cri-o.sh
@@ -15,49 +15,17 @@
 
 set -euo pipefail
 
-COMMIT_ID="d7cc66fe80c51a197d13a642ad5bddd9dc7e9f74"
+COMMIT_ID=c4c89c6e426bf574c345465f7fab81b56fcfb1a8
+TAG=v1.23.1
 
-curl "https://raw.githubusercontent.com/cri-o/cri-o/${COMMIT_ID}/scripts/get" | bash
+curl "https://raw.githubusercontent.com/cri-o/cri-o/$COMMIT_ID/scripts/get" | bash -s -- -t "$TAG"
 
 . /etc/os-release
 
 if [[ $ID == fedora ]]; then
-    chcon -u system_u -r object_r -t container_runtime_exec_t \
-        /usr/local/bin/crio \
-        /usr/local/bin/crio-status \
-        /usr/local/bin/runc \
-        /usr/local/bin/crun
-
-    chcon -u system_u -r object_r -t bin_t \
-        /usr/local/bin/conmon \
-        /usr/local/bin/crictl \
-        /usr/local/bin/pinns
-
-    chcon -R -u system_u -r object_r -t bin_t /opt/cni/bin
-
     mkdir -p /var/lib/kubelet
     chcon -R -u system_u -r object_r -t var_lib_t /var/lib/kubelet
-
-    cat <<EOT >>/etc/crio/crio.conf.d/30-selinux.conf
-[crio.runtime]
-selinux = true
-EOT
-
-    chcon -R -u system_u -r object_r -t container_config_t \
-        /etc/crio \
-        /etc/crio/crio.conf \
-        /usr/local/share/oci-umount/oci-umount.d/crio-umount.conf
+    printf '[crio.runtime]\nselinux = true' >/etc/crio/crio.conf.d/30-selinux.conf
 fi
 
-if [[ $ID == ubuntu ]]; then
-    rm /etc/cni/net.d/10-crio-bridge.conf
-    printf '[crio.network]\ncni_default_network = "weave"' >/etc/crio/crio.conf.d/01-net.conf
-fi
-
-systemctl enable crio.service
-
-if [[ $ID == fedora ]]; then
-    restorecon /usr/local/lib/systemd/system/crio.service
-fi
-
-systemctl start crio.service
+systemctl enable --now crio.service
