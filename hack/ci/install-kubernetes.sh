580ae7ce5a3d2e283de4fabe08b1f8e5d2b5837d
Sascha Grunert <sgrunert@redhat.com>
2022-03-18 11:37:09 +0100

    tree 8dc2c8153dd5b607cfb0b43d605f98ad7a55eae6 parent 32a49abd3e3d73fde39c5e6739bec6e2722fe5c1 author Sascha Grunert <sgrunert@redhat.com> 1647599829 +0100 committer Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com> 1647855069 -0700
    Bump Kubernetes to v1.23.5 and CRI-O to v1.23.2
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/ci/install-kubernetes.sh
+++ hack/ci/install-kubernetes.sh
@@ -19,7 +19,7 @@ ENVFILE=$(dirname "${BASH_SOURCE[0]}")/env-fedora.sh
 . "$ENVFILE"
 
 K8SPATH="$GOPATH/src/k8s.io"
-VERSION=v1.23.4
+VERSION=v1.23.5
 
 download-kubernetes() {
     export KUBERNETES_RELEASE=$VERSION
