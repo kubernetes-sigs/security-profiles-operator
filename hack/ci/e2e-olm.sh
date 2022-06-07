904460f5b86350054a04afba25264d4f710c308c
Sascha Grunert <sgrunert@redhat.com>
2022-06-07 13:38:53 +0200

    tree cf17dff9705cbb484b1fa25edf809c2d26568627 parent b19909784f3d5eae7d7aeacc053c9056cd7b7c2b author Sascha Grunert <sgrunert@redhat.com> 1654601933 +0200 committer Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com> 1654607817 -0700
    Back to v0.4.4-dev
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/ci/e2e-olm.sh
+++ hack/ci/e2e-olm.sh
@@ -62,7 +62,7 @@ function deploy_spo() {
     kubectl -ncert-manager wait --for condition=ready pod -l app.kubernetes.io/instance=cert-manager
 
     # let's roll..
-    sed -i "s#k8s.gcr.io/security-profiles-operator/security-profiles-operator-catalog:v0.4.3#${CATALOG_IMG}#g" examples/olm/install-resources.yaml
+    sed -i "s#gcr.io/k8s-staging-sp-operator/security-profiles-operator-catalog:latest#${CATALOG_IMG}#g" examples/olm/install-resources.yaml
     kubectl create -f examples/olm/install-resources.yaml
 }
 
