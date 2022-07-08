51faa76e55ab2d105c5b3ce06c8ab318d92722eb
Sascha Grunert <sgrunert@redhat.com>
2022-07-08 10:07:56 +0200

    Update Kubernetes to v1.24.3
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/ci/install-kubernetes.sh
+++ hack/ci/install-kubernetes.sh
@@ -19,7 +19,7 @@ ENVFILE=$(dirname "${BASH_SOURCE[0]}")/env-fedora.sh
 . "$ENVFILE"
 
 K8SPATH="$GOPATH/src/k8s.io"
-VERSION=v1.24.0
+VERSION=v1.24.3
 
 download-kubernetes() {
     export KUBERNETES_RELEASE=$VERSION
