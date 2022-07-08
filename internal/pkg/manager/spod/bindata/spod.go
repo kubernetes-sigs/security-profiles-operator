a704af9586008c502973587892920dbe410efd50
Sascha Grunert <sgrunert@redhat.com>
2022-07-08 10:16:48 +0200

    Update kube-rbac-proxy to v0.13.0
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- internal/pkg/manager/spod/bindata/spod.go
+++ internal/pkg/manager/spod/bindata/spod.go
@@ -52,7 +52,7 @@ const (
 	SelinuxdPrivateDir                         = "/var/run/selinuxd"
 	SelinuxdSocketPath                         = SelinuxdPrivateDir + "/selinuxd.sock"
 	SelinuxdDBPath                             = SelinuxdPrivateDir + "/selinuxd.db"
-	MetricsImage                               = "quay.io/brancz/kube-rbac-proxy:v0.11.0"
+	MetricsImage                               = "quay.io/brancz/kube-rbac-proxy:v0.13.0"
 	sysKernelDebugPath                         = "/sys/kernel/debug"
 	InitContainerIDNonRootenabler              = 0
 	InitContainerIDSelinuxSharedPoliciesCopier = 1
