cb426d6d90526a873e8ccf808aff4dedf872e606
Sascha Grunert <sgrunert@redhat.com>
2022-10-05 10:29:24 +0200

    tree 278b14021f05d4162ee16132da2f0fa35d9918d2 parent 87eab44234a547dbbdbd67fe494f1c5fe0ae0c17 author Sascha Grunert <sgrunert@redhat.com> 1664958564 +0200 committer Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com> 1664976963 -0700
    Switch to libbpf_1 dependency in nix
    
    We now have an official libbpf version in upstream nix after the merge
    of: https://github.com/NixOS/nixpkgs/pull/193591
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,14 +4,4 @@ self: super:
     doCheck = false;
     dontDisableStatic = true;
   });
-  # TODO: remove when https://github.com/NixOS/nixpkgs/pull/187978 got merged
-  libbpf = super.libbpf.overrideAttrs (x: {
-    version = "1.0.0";
-    src = super.fetchFromGitHub {
-      owner = "libbpf";
-      repo = "libbpf";
-      rev = "v1.0.0";
-      sha256 = "sha256-JU/Ia85V4L1DtwRcIn9OF/qt52hYSQhkw2Iz2ovEwqo=";
-    };
-  });
 }
