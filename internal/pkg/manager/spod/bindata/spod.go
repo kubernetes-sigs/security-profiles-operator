b3e8b745ade8831cc2cdd3470c28419edc3aa2f5
Sascha Grunert <sgrunert@redhat.com>
2023-01-23 11:23:59 +0100

    Switch to `gcr.io/kubebuilder/kube-rbac-proxy`
    The old location does not seem to be the official one, so we switch over
    to the new one as well as adapt the dependency verification.
    
    Fixes https://github.com/kubernetes-sigs/security-profiles-operator/issues/1423
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- internal/pkg/manager/spod/bindata/spod.go
+++ internal/pkg/manager/spod/bindata/spod.go
@@ -53,7 +53,7 @@ const (
 	SelinuxdPrivateDir                         = "/var/run/selinuxd"
 	SelinuxdSocketPath                         = SelinuxdPrivateDir + "/selinuxd.sock"
 	SelinuxdDBPath                             = SelinuxdPrivateDir + "/selinuxd.db"
-	MetricsImage                               = "quay.io/brancz/kube-rbac-proxy:v0.13.1"
+	MetricsImage                               = "gcr.io/kubebuilder/kube-rbac-proxy:v0.13.1"
 	sysKernelDebugPath                         = "/sys/kernel/debug"
 	InitContainerIDNonRootenabler              = 0
 	InitContainerIDSelinuxSharedPoliciesCopier = 1
