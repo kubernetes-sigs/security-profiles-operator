a98c08f5c9837e88fcba44767e51e82a887d08a1
Sascha Grunert <sgrunert@redhat.com>
2023-03-08 11:21:17 +0100

    tree 5f0d9eb0742fa3e60f5da6f3fbdb96cd49669cc1 parent 20058eb0fefc90096862cc5401ad90b68936b607 author Sascha Grunert <sgrunert@redhat.com> 1678270877 +0100 committer Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com> 1678289113 -0800
    Bump cosign to v2.0.0
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/ci/install-cri-o.sh
+++ hack/ci/install-cri-o.sh
@@ -17,24 +17,26 @@ set -euo pipefail
 
 TAG=v1.26.1
 
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH="$HOME/go"
-export GOBIN="$GOPATH/bin"
+curl_retry() {
+    curl -sSfL --retry 5 --retry-delay 3 "$@"
+}
 
 # We need cosign as well as the bom tool here because the CRI-O installation
 # script will automatically verify the signatures based on their existence in
 # $PATH.
-COSIGN_VERSION=v1.13.1
-go install github.com/sigstore/cosign/cmd/cosign@$COSIGN_VERSION
-cp "$GOBIN/cosign" /usr/bin
+COSIGN_VERSION=v2.0.0
+curl_retry -o /usr/bin/cosign \
+    https://github.com/sigstore/cosign/releases/download/$COSIGN_VERSION/cosign-linux-amd64
+chmod +x /usr/bin/cosign
 cosign version
 
 BOM_VERSION=v0.4.1
-go install sigs.k8s.io/bom/cmd/bom@$BOM_VERSION
-cp "$GOBIN/bom" /usr/bin
+curl_retry -o /usr/bin/bom \
+    https://github.com/kubernetes-sigs/bom/releases/download/$BOM_VERSION/bom-linux-amd64
+chmod +x /usr/bin/bom
 bom version
 
-curl -sSfL --retry 5 --retry-delay 3 "https://raw.githubusercontent.com/cri-o/cri-o/$TAG/scripts/get" |
+curl_retry "https://raw.githubusercontent.com/cri-o/cri-o/main/scripts/get" |
     bash -s -- -t "$TAG"
 
 . /etc/os-release
