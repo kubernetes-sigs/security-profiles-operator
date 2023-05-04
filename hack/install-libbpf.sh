d24d91137e0860949c07093090cde0faf0ddbd1e
Sascha Grunert <sgrunert@redhat.com>
2023-05-04 09:17:47 +0200

    Bump libbpf
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/install-libbpf.sh
+++ hack/install-libbpf.sh
@@ -15,7 +15,7 @@
 
 set -euo pipefail
 
-VERSION=1.0.1
+VERSION=1.2.0
 curl -sSfL --retry 5 --retry-delay 3 \
     "https://github.com/libbpf/libbpf/archive/refs/tags/v$VERSION.tar.gz" -o- |
     tar xfz -
