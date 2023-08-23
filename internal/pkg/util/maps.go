ef892157a1403c164d66bf84b74a69d7a760ca2b
novaesis <novaesis@users.noreply.github.com>
2023-08-23 08:31:40 -0400

    Added missing header and corrected godot error

--- internal/pkg/util/maps.go
+++ internal/pkg/util/maps.go
@@ -1,8 +1,24 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package util
 
 // To avoid having to use experimental library imports, the below is taken from https://cs.opensource.google/go/x/exp/+/master:maps/maps.go
 // Keys returns the keys of the map m.
-// The keys will be in an indeterminate order
+// The keys will be in an indeterminate order.
 func MapKeys[M ~map[K]V, K comparable, V any](m M) []K {
 	r := make([]K, 0, len(m))
 	for k := range m {
