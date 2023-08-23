eba6bbf0c2c66e74d80b179a7f01c7334638a1be
Sascha Grunert <sgrunert@redhat.com>
2023-08-23 09:50:57 +0200

    Switch to Kubernetes v1.28.0 in CI
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/ci/install-kubernetes.sh
+++ hack/ci/install-kubernetes.sh
@@ -19,7 +19,7 @@ ENVFILE=$(dirname "${BASH_SOURCE[0]}")/env-fedora.sh
 . "$ENVFILE"
 
 K8SPATH="$GOPATH/src/k8s.io"
-VERSION=v1.27.2
+VERSION=v1.28.0
 
 download-kubernetes() {
     export KUBERNETES_RELEASE=$VERSION
