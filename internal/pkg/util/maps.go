09c6f0523885e6427e61d1f84b9966efe50eb206
novaesis <novaesis@users.noreply.github.com>
2023-08-23 08:40:11 -0400

    Fix length of line in maps.go

--- internal/pkg/util/maps.go
+++ internal/pkg/util/maps.go
@@ -16,7 +16,9 @@ limitations under the License.
 
 package util
 
-// To avoid having to use experimental library imports, the below is taken from https://cs.opensource.google/go/x/exp/+/master:maps/maps.go
+// To avoid having to use experimental library imports, the below is taken from 
+// https://cs.opensource.google/go/x/exp/+/master:maps/maps.go
+
 // Keys returns the keys of the map m.
 // The keys will be in an indeterminate order.
 func MapKeys[M ~map[K]V, K comparable, V any](m M) []K {
