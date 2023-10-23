c51a755e7ba935a28b857b1405fc5cd32394b13a
Sascha Grunert <sgrunert@redhat.com>
2023-10-23 12:28:45 +0200

    Update crun to 1.10
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- test/tc_base_profiles_test.go
+++ test/tc_base_profiles_test.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	baseProfileNameRunc = "runc-v1.1.9"
-	baseProfileNameCrun = "crun-v1.9.2"
+	baseProfileNameCrun = "crun-v1.10"
 )
 
 func (e *e2e) testCaseBaseProfile([]string) {
