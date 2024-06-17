e6d8e5f89e29d466f03fe15c5b881ff48a0ff8b4
Cosmin Cojocar <ccojocar@google.com>
2024-06-17 07:33:53 +0000

    tree a516ad6323e256766084b10b7c9b43735f79f291 parent ff4783e3d4edc567c6502d236f781ba0a71a4888 author Cosmin Cojocar <ccojocar@google.com> 1718609633 +0000 committer Kubernetes Prow Robot <20407524+k8s-ci-robot@users.noreply.github.com> 1719909359 -0700
    Set the env variable in each prepare function
    
    Set the E2E_TEST_BPF_LSM_ENABLED env variabl in each prepare function
    because it seems that cannot be set globally in parallel tests.
    
    Change-Id: Ia545b2a44e9a0c50e10af0c61db77b7002415e9a
    Signed-off-by: Cosmin Cojocar <ccojocar@google.com>

--- internal/pkg/daemon/bpfrecorder/bpfrecorder_test.go
+++ internal/pkg/daemon/bpfrecorder/bpfrecorder_test.go
@@ -651,6 +651,10 @@ func TestApparmorForProfile(t *testing.T) {
 	}{
 		{ // Success
 			prepare: func(sut *BpfRecorder, mock *bpfrecorderfakes.FakeImpl) {
+				// This is required to enable the unit tests when they are executed on an
+				// Linux OS without BPF_LSM module enabled.
+				t.Setenv("E2E_TEST_BPF_LSM_ENABLED", "1")
+
 				mock.GoArchReturns(validGoArch)
 				_, err := sut.Start(context.Background(), &api.EmptyRequest{})
 				require.Nil(t, err)
@@ -683,6 +687,10 @@ func TestApparmorForProfile(t *testing.T) {
 		},
 		{ // Success only for right mntns
 			prepare: func(sut *BpfRecorder, mock *bpfrecorderfakes.FakeImpl) {
+				// This is required to enable the unit tests when they are executed on an
+				// Linux OS without BPF_LSM module enabled.
+				t.Setenv("E2E_TEST_BPF_LSM_ENABLED", "1")
+
 				mock.GoArchReturns(validGoArch)
 				_, err := sut.Start(context.Background(), &api.EmptyRequest{})
 				require.Nil(t, err)
@@ -732,6 +740,10 @@ func TestApparmorForProfile(t *testing.T) {
 		},
 		{ // no PID for container
 			prepare: func(sut *BpfRecorder, mock *bpfrecorderfakes.FakeImpl) {
+				// This is required to enable the unit tests when they are executed on an
+				// Linux OS without BPF_LSM module enabled.
+				t.Setenv("E2E_TEST_BPF_LSM_ENABLED", "1")
+
 				mock.GoArchReturns(validGoArch)
 				_, err := sut.Start(context.Background(), &api.EmptyRequest{})
 				require.Nil(t, err)
@@ -745,10 +757,6 @@ func TestApparmorForProfile(t *testing.T) {
 
 		mock := &bpfrecorderfakes.FakeImpl{}
 		sut.impl = mock
-
-		// This is required to enable the unit tests when they are executed on an
-		// Linux OS without BPF_LSM module enabled.
-		t.Setenv("E2E_TEST_BPF_LSM_ENABLED", "1")
 
 		tc.prepare(sut, mock)
 
