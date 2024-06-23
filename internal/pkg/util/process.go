a3544789104c79a31a161557743d9c8e6f2f79bd
Cosmin Cojocar <ccojocar@google.com>
2024-06-23 12:42:06 +0000

    Fix the spoc e2e tests when the proc is started externally
    Change-Id: I1c0502d9a7972868d15b32d06ce318898686a7ab
    Signed-off-by: Cosmin Cojocar <ccojocar@google.com>

---
+++ internal/pkg/util/process.go
@@ -0,0 +1,114 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	defaultPID  = -1
+	processRoot = "/proc"
+)
+
+// ErrEmptyPIDName indicates an erorr for a PID with an empty process name
+var ErrEmptyPIDName = errors.New("process Name of given PID is empty")
+
+var procRegexp = regexp.MustCompile(`[/]*proc/[\d]+/cmdline`)
+
+// ProcessIDByName is looking up the PID by process name
+func ProcessIDByName(name string) (int, error) {
+	p := &proc{
+		name: name,
+		pid:  defaultPID,
+	}
+	return p.findPIDByName(processRoot)
+}
+
+type proc struct {
+	name string
+	pid  int
+}
+
+func (p *proc) findPIDByName(root string) (int, error) {
+	err := filepath.Walk(root, p.walkProc)
+	if err != nil {
+		return -1, fmt.Errorf("looking for pid of process %q: %w", p.name, err)
+	}
+	if p.pid != defaultPID {
+		return p.pid, nil
+	}
+	return -1, fmt.Errorf("could not find a valid pid for process name %q", p.name)
+}
+
+func (p *proc) walkProc(path string, info os.FileInfo, err error) error {
+	// Return already if there is an existing error due to for instance
+	// insufficient privileges.
+	if err != nil {
+		return nil
+	}
+
+	// Skip paths which doesn't look like /proc/<pid>cmdline.
+	if !procRegexp.Match([]byte(path)) {
+		return nil
+	}
+
+	pid, err := parsePID(path)
+	if err != nil {
+		return fmt.Errorf("parsing PID from path %s: %w", path, err)
+	}
+
+	name, err := parseName(path)
+	if err != nil {
+		// skip pids without empty command and keep going
+		if err == ErrEmptyPIDName {
+			return nil
+		}
+		return fmt.Errorf("parsing process name from path %s: %w", path, err)
+	}
+
+	// Update the PID if we found the process
+	if strings.HasPrefix(name, p.name) {
+		p.pid = pid
+	}
+
+	return nil
+}
+
+func parsePID(dir string) (int, error) {
+	pidDir, _ := path.Split(dir)
+	return strconv.Atoi(path.Base(pidDir))
+}
+
+func parseName(path string) (string, error) {
+	f, err := os.ReadFile(path)
+	if err != nil {
+		return "", fmt.Errorf("reading from %s: %w", path, err)
+	}
+	name := strings.TrimSpace(string(f))
+	if len(name) > 0 {
+		return name, nil
+	}
+	return "", ErrEmptyPIDName
+}
