206bd230df13dfc66f16942fed2dc6fa1dc6e273
Vincent Shen <wenshen@redhat.com>
2025-01-15 01:41:31 -0800

    Convert python script to go instead
    We are doing this to enable hermetic build

--- bundle-hack/update_bundle_namespace.sh
+++ bundle-hack/update_bundle_namespace.sh
@@ -1,8 +1,8 @@
 
 # This script is used to update the namespace in the bundle manifests to openshift-security-profiles
 echo "Replacing default namespace in RBAC manifests"
-sed -i "s#namespace: security-profiles-operator#namespace: openshift-security-profiles#g" ./manifests/spo-metrics-client_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml
-sed -i "s#namespace: security-profiles-operator#namespace: openshift-security-profiles#g" ./manifests/spo-webhook_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml
-sed -i "s#namespace: security-profiles-operator#namespace: openshift-security-profiles#g" ./manifests/spod_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml
-sed -i "s#namespace: security-profiles-operator#namespace: openshift-security-profiles#g" ./manifests/spod_rbac.authorization.k8s.io_v1_rolebinding.yaml
+sed -i "s#namespace: security-profiles-operator#namespace: openshift-security-profiles#g" ../bundle/manifests/spo-metrics-client_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml
+sed -i "s#namespace: security-profiles-operator#namespace: openshift-security-profiles#g" ../bundle/manifests/spo-webhook_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml
+sed -i "s#namespace: security-profiles-operator#namespace: openshift-security-profiles#g" ../bundle/manifests/spod_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml
+sed -i "s#namespace: security-profiles-operator#namespace: openshift-security-profiles#g" ../bundle/manifests/spod_rbac.authorization.k8s.io_v1_rolebinding.yaml
 
