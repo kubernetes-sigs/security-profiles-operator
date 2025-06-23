8b4f467e6809f969447ec74bc3afd75c1f40b51f
Neeraj Krishna Gopalakrishna <ngopalak@redhat.com>
2025-06-23 09:52:57 +0530

    Testing logging

--- internal/pkg/daemon/enricher/process_test.go
+++ internal/pkg/daemon/enricher/process_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package enricher
 
 import "testing"
@@ -13,10 +29,22 @@ func Test_extractSPORequestUID(t *testing.T) {
 		foundWant bool
 	}{
 		{
-			name:      "test basic",
+			name:      "Basic test with cmdline having SPO_EXEC_REQUEST_UID",
 			args:      args{input: "env SPO_EXEC_REQUEST_UID=dbbf5fca-c955-4922-99d2-27a50212071c ls"},
 			want:      "dbbf5fca-c955-4922-99d2-27a50212071c",
 			foundWant: true,
+		},
+		{
+			name:      "Test with no value",
+			args:      args{input: "ls"},
+			want:      "",
+			foundWant: false,
+		},
+		{
+			name:      "Test with other env values",
+			args:      args{input: "env INVALID=dbbf5fca-c955-4922-99d2-27a50212071c ls"},
+			want:      "",
+			foundWant: false,
 		},
 	}
 	for _, tt := range tests {
