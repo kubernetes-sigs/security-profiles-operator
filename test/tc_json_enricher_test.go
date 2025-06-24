c6c44c212958f5dc9c12c40384777f3921c7e992
Neeraj Krishna Gopalakrishna <ngopalak@redhat.com>
2025-06-24 20:50:27 +0530

    Fix kubectl debug for JSON Enricher

--- test/tc_json_enricher_test.go
+++ test/tc_json_enricher_test.go
@@ -193,16 +193,17 @@ spec:
 
 	time.Sleep(10 * time.Second)
 
-	e.kubectl("debug", "-it", podName, "--image", "busybox:latest", "--", "sleep", "6")
-	e.kubectl("exec", "-it", podName, "--", "sleep", "5")
+	e.logf("kubectl debug and sleep for 6 seconds")
+	e.kubectl("debug", "-i", podName, "--image", "busybox:latest", "--", "sleep", "6")
+	e.logf("kubectl exec and sleep for 5 seconds")
+	e.kubectl("exec", "-i", podName, "--", "sleep", "5")
+	e.logf("kubectl exec and print env")
 	envOutput := e.kubectl("exec", "-it", podName, "--", "env")
 	e.Contains(envOutput, "SPO_EXEC_REQUEST_UID")
+	e.logf("The env output has SPO_EXEC_REQUEST_UID")
 
 	// wait for at least one component of the expected logs to appear
 	e.waitForJsonEnricherLogs(since, regexp.MustCompile(`(?m)"requestUID"`))
-
-	e.logf("Wait for the audit lines to come within 15 seconds")
-	time.Sleep(15 * time.Second)
 	e.logf("Checking JSON enricher output")
 	output := e.kubectlOperatorNS("logs", "-l", "name=spod", "-c", "json-enricher")
 
