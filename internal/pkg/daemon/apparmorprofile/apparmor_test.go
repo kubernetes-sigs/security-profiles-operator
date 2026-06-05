4308229a5027209190110546f6edca931a293f80
Cosmin Cojocar <ccojocar@google.com>
2026-06-05 15:06:55 +0000

    Fix lint warnings
    Signed-off-by: Cosmin Cojocar <ccojocar@google.com>

--- internal/pkg/daemon/apparmorprofile/apparmor_test.go
+++ internal/pkg/daemon/apparmorprofile/apparmor_test.go
@@ -24,9 +24,8 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/stretchr/testify/require"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/security-profiles-operator/api/apparmorprofile/v1alpha1"
 	profilebasev1alpha1 "sigs.k8s.io/security-profiles-operator/api/profilebase/v1alpha1"
 	sec "sigs.k8s.io/security-profiles-operator/api/seccompprofile/v1beta1"
