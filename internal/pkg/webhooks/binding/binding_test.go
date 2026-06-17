ff91922b8f9dbb643006970a490699f1c902eda3
Cosmin Cojocar <ccojocar@google.com>
2026-06-17 07:53:57 +0000

    Fix formatting
    Signed-off-by: Cosmin Cojocar <ccojocar@google.com>

--- internal/pkg/webhooks/binding/binding_test.go
+++ internal/pkg/webhooks/binding/binding_test.go
@@ -204,12 +204,11 @@ func TestHandle(t *testing.T) {
 					Object: runtime.RawExtension{
 						Raw: func() []byte {
 							podWithSecurityContext := testPod.DeepCopy()
-							podWithSecurityContext.Spec.Containers[0].SecurityContext =
-								&corev1.SecurityContext{
-									SeccompProfile: &corev1.SeccompProfile{
-										Type: corev1.SeccompProfileTypeUnconfined,
-									},
-								}
+							podWithSecurityContext.Spec.Containers[0].SecurityContext = &corev1.SecurityContext{
+								SeccompProfile: &corev1.SeccompProfile{
+									Type: corev1.SeccompProfileTypeUnconfined,
+								},
+							}
 							b, err := json.Marshal(podWithSecurityContext)
 							require.NoError(t, err)
 
@@ -292,12 +291,11 @@ func TestHandle(t *testing.T) {
 					Object: runtime.RawExtension{
 						Raw: func() []byte {
 							podWithSecurityContext := testPod.DeepCopy()
-							podWithSecurityContext.Spec.Containers[0].SecurityContext =
-								&corev1.SecurityContext{
-									SELinuxOptions: &corev1.SELinuxOptions{
-										Type: "unconfined",
-									},
-								}
+							podWithSecurityContext.Spec.Containers[0].SecurityContext = &corev1.SecurityContext{
+								SELinuxOptions: &corev1.SELinuxOptions{
+									Type: "unconfined",
+								},
+							}
 							b, err := json.Marshal(podWithSecurityContext)
 							require.NoError(t, err)
 
@@ -423,12 +421,11 @@ func TestHandle(t *testing.T) {
 					Object: runtime.RawExtension{
 						Raw: func() []byte {
 							podWithSecurityContext := testPod.DeepCopy()
-							podWithSecurityContext.Spec.Containers[0].SecurityContext =
-								&corev1.SecurityContext{
-									AppArmorProfile: &corev1.AppArmorProfile{
-										Type: corev1.AppArmorProfileTypeUnconfined,
-									},
-								}
+							podWithSecurityContext.Spec.Containers[0].SecurityContext = &corev1.SecurityContext{
+								AppArmorProfile: &corev1.AppArmorProfile{
+									Type: corev1.AppArmorProfileTypeUnconfined,
+								},
+							}
 							b, err := json.Marshal(podWithSecurityContext)
 							require.NoError(t, err)
 
